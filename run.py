75a0decdc9fac19a76aa0321f057cd62a61300ef
Gamal <gamalalmaqtary6838@gmail.com>
2026-04-11 16:18:31 +0300

    k

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 from fastapi import FastAPI
 import uvicorn
@@ -7,8 +8,6 @@ from app.bot.dispatcher import dp
 from app.bot.routers import main_router
 
 app = FastAPI()
-
-
 
 @app.on_event("startup")
 async def startup():
@@ -20,4 +19,11 @@ def root():
     return {"status": "running"}
 
 if __name__ == "__main__":
-    uvicorn.run("run:app", port=8000, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "run:app",
+        host="0.0.0.0",   # 🔥 مهم جدًا
+        port=port,
+        reload=False
+    )
