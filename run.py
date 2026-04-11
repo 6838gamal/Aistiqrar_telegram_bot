971d3e05cd1be83416983e1811d6ef1b4cceafad
Gamal <gamalalmaqtary6838@gmail.com>
2026-04-11 16:09:24 +0300

    j

--- run.py
+++ run.py
@@ -8,10 +8,11 @@ from app.bot.routers import main_router
 
 app = FastAPI()
 
-dp.include_router(main_router)
+
 
 @app.on_event("startup")
 async def startup():
+    dp.include_router(main_router)
     asyncio.create_task(dp.start_polling(bot))
 
 @app.get("/")
