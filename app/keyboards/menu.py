42a97079214012e3ba4fc2af7feb51d25e123490
Gamal <gamalalmaqtary6838@gmail.com>
2026-04-11 20:42:44 +0300

    h

--- app/keyboards/menu.py
+++ app/keyboards/menu.py
@@ -1,24 +1,28 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from app.utils.translator import translator
 
 
 def home_menu(lang="ar"):
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text=translator.t("menu_start", lang), callback_data="page_start")],
-            [InlineKeyboardButton(text=translator.t("menu_job", lang), callback_data="page_job")],
-            [InlineKeyboardButton(text=translator.t("menu_profile", lang), callback_data="page_profile")],
-            [InlineKeyboardButton(text=translator.t("menu_categories", lang), callback_data="page_categories")],
-            [InlineKeyboardButton(text=translator.t("menu_suggest", lang), callback_data="page_suggest")],
-            [InlineKeyboardButton(text=translator.t("menu_invite", lang), callback_data="page_invite")],
-            [InlineKeyboardButton(text=translator.t("menu_help", lang), callback_data="page_help")]
-        ]
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text=translator.t("menu_start", lang))],
+            [KeyboardButton(text=translator.t("menu_job", lang))],
+            [KeyboardButton(text=translator.t("menu_profile", lang))],
+            [KeyboardButton(text=translator.t("menu_categories", lang))],
+            [KeyboardButton(text=translator.t("menu_suggest", lang))],
+            [KeyboardButton(text=translator.t("menu_invite", lang))],
+            [KeyboardButton(text=translator.t("menu_help", lang))],
+        ],
+        resize_keyboard=True,
+        one_time_keyboard=False
     )
 
 
 def back_button():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="🔙 رجوع", callback_data="back_home")]
-        ]
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="🔙 رجوع")]
+        ],
+        resize_keyboard=True,
+        one_time_keyboard=False
     )
